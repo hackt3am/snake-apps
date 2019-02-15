6791ef0bcc4b40800ddf890252e73e15d09ef2d8
simmule-turner <31528536+simmule-turner@users.noreply.github.com>
2019-02-14 20:24:17 -0800

    Update Android.mk

--- Chronus/Android.mk
+++ Chronus/Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE := Chronous
+LOCAL_MODULE := Chronus
 LOCAL_SRC_FILES := com.dvtonder.chronus_173175_apps.evozi.com.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
