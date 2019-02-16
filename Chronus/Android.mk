7c10d93549963d981e2ba1423814b065e1ed9044
* <simmule.turner@gmail.com>
2019-02-15 19:47:51 -0800

    Latest version of Chronus

--- Chronus/Android.mk
+++ Chronus/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := Chronus
-LOCAL_SRC_FILES := com.dvtonder.chronus_173175_apps.evozi.com.apk
+LOCAL_SRC_FILES := Chronus_Home_Lock_Widgets_v12.4.2_apkpure.com.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_CERTIFICATE := PRESIGNED
