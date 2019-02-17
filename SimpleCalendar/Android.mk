dfdec291e1ee6fdfa4bd64f516e6d6b8c0954516
* <simmule.turner@gmail.com>
2019-02-16 19:30:39 -0800

    Update to SimpleCalendar.

--- SimpleCalendar/Android.mk
+++ SimpleCalendar/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := SimpleCalendar
-LOCAL_SRC_FILES := com.simplemobiletools.calendar.pro_142.apk
+LOCAL_SRC_FILES := Calendar_6.3.0.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_CERTIFICATE := PRESIGNED
