5f967029fca0abca7d2e4e46f28ef64e01f9c89b
* <simmule.turner@gmail.com>
2019-03-13 01:06:11 -0700

    Latest versions of Markor and NetGuard.

--- Markor/Android.mk
+++ Markor/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := Markor
-LOCAL_SRC_FILES := net.gsantner.markor_50.apk
+LOCAL_SRC_FILES := net.gsantner.markor_54.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_CERTIFICATE := PRESIGNED
